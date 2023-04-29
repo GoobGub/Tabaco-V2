a5c943c6577a85c1c89e3133b8cb01b6d3f43a97
GoobGub <129594730+GoobGub@users.noreply.github.com>
2023-04-29 18:18:27 +0930

    Add files via upload

---
+++ Tabaco V3/Tabaco Ciggies/Tabaco V2/Tools/BotNetDDOS.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# Set the URL of the GitHub page you want to open
+url = "https://github.com/skavngr/netbot"
+
+# Open the URL in your default web browser
+webbrowser.open_new_tab(url)
---
+++ Tabaco V3/Tabaco Ciggies/Tabaco V2/Tools/Discord Spam/spammers/__init__.py
@@ -0,0 +1,7 @@
+from .assetsManager import *
+from .banner import *
+from .color import *
+from .friendRequester import *
+from .guildLeaver import *
+from .messageSpammer import *
+from .voicechannelJoiner import *
