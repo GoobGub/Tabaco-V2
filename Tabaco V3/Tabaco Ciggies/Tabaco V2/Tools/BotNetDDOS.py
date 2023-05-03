84edd36dfec818af09b69e91df6cf798c904aa08
GoobGub <129594730+GoobGub@users.noreply.github.com>
2023-05-03 15:47:48 +0930

    Delete Tabaco V3/Tabaco Ciggies directory

--- Tabaco V3/Tabaco Ciggies/Tabaco V2/Tools/BotNetDDOS.py
+++
@@ -1,7 +0,0 @@
-import webbrowser
-
-# Set the URL of the GitHub page you want to open
-url = "https://github.com/skavngr/netbot"
-
-# Open the URL in your default web browser
-webbrowser.open_new_tab(url)
--- Tabaco V3/Tabaco Ciggies/Tabaco V2/Tools/Discord Spam.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Change directory to the Token Gen folder
-os.chdir('Tools/Discord Spam')
-
-# Run the TokenGen.py script
-os.system('python DiscordSpam.py')
